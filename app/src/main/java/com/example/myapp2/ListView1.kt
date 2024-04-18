50684f330f0a828b33a934a3bc2951465c3eea4c
kanned1079 <kanned1079@gmail.com>
2024-04-18 17:18:03 +0800

    update

--- app/src/main/java/com/example/myapp2/ListView1.kt
+++ app/src/main/java/com/example/myapp2/ListView1.kt
@@ -5,13 +5,17 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.TextView
 
 class ListView1 : AppCompatActivity() {
     lateinit var myColorlist: ListView
     lateinit var myStylelist: ListView
     lateinit var myText: TextView
+    lateinit var myText2: TextView
+    lateinit var myImageview1 :ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view1)
@@ -19,6 +23,10 @@ class ListView1 : AppCompatActivity() {
         myColorlist = findViewById(R.id.listview1)
         myStylelist = findViewById(R.id.listview2)
         myText = findViewById(R.id.tv1)
+
+        myImageview1 = findViewById(R.id.imgView1)
+        myText2 = findViewById(R.id.ed2)
+
 
         var colors = arrayListOf("红", "绿", "蓝")
         myColorlist.choiceMode = 1
@@ -52,6 +60,19 @@ class ListView1 : AppCompatActivity() {
             myText.setTypeface(Typeface.create(myText.typeface, textStyle)) // 得用这个
 //            myText.setTypeface(null, textStyle) // 这个不行
         }
+
+        var imgsIDs = intArrayOf(R.drawable.surprised, R.drawable.expression, R.drawable.strange)
+        var listContent = arrayListOf("google", "tel:23434", "nnutc")
+        var mySimpleMap: ArrayList<Map<String, Any>> = ArrayList()
+        for (i in listContent.indices) {
+            var item = HashMap<String, Any>()
+            item["itemPic"] = imgsIDs[i]
+            item["itemStyle"] = listContent[i]
+            mySimpleMap.add(item)
+        }
+        var from = arrayListOf("itemPic", "itemStyle")
+//        var to = intArrayOf(R.id.imgView1, R.id.)
+//        var myWebSimpleAdap = SimpleAdapter(this, mySimpleMap, R.id.tv2, from, to)
 
     }
 
---
+++ app/src/main/java/com/example/myapp2/MyIntentList1.kt
@@ -0,0 +1,11 @@
+package com.example.myapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyIntentList1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_intent_list1)
+    }
+}
