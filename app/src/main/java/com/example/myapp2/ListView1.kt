4d0c44c48e1ca4c569693a6377694b75068419f6
kanned1079 <kanned1079@gmail.com>
2024-04-19 16:03:55 +0800

    update0419

--- app/src/main/java/com/example/myapp2/ListView1.kt
+++ app/src/main/java/com/example/myapp2/ListView1.kt
@@ -1,7 +1,9 @@
 package com.example.myapp2
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -9,6 +11,7 @@ import android.widget.ImageView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import android.widget.TextView
+import android.widget.Toast
 
 class ListView1 : AppCompatActivity() {
     lateinit var myColorlist: ListView
@@ -66,7 +69,7 @@ class ListView1 : AppCompatActivity() {
 
         // 这里开始是listView3的内容
         var imgsIDs = arrayOf<Int>(R.drawable.surprised, R.drawable.expression, R.drawable.strange)
-        var listContent = arrayListOf<String>("aaaaa", "bbbbb", "ccccc")
+        var listContent = arrayListOf<String>("跳转到计算器", "跳转到网页", "跳转到电话")
         var btList = arrayOf("按钮1", "按钮2", "按钮3")
         var newsList: ArrayList<Map<String, Any>> = ArrayList()
         for (i in listContent.indices) {
@@ -79,7 +82,32 @@ class ListView1 : AppCompatActivity() {
         var from = arrayOf("itemPic", "itemStyle", "itemBtn")
         var to = intArrayOf(R.id.iv_new_image, R.id.tv_news_title, R.id.iv_news_btn)
         var mySimpleAdapter:SimpleAdapter = SimpleAdapter(this, newsList, R.layout.news, from, to)
+        // 如果要点击里面的按钮那就不能用SimpleAdapter
         newsLists.adapter = mySimpleAdapter
+
+        newsLists.setOnItemClickListener { adapterView, view, i, l ->
+            when(i) {
+//                Toast.makeText(this, i.toString(), Toast.LENGTH_SHORT).show()
+                //
+                0 -> {
+                    // 显式Intent 跳转到Calculator
+                    var toCal = Intent(this, Calculator::class.java)
+                    startActivity(toCal)}
+                1 -> {
+                    var toWeb = Intent(Intent.ACTION_VIEW, Uri.parse("https://ikanned.com:55444/"))
+                    startActivity(toWeb)
+                }
+                2 -> {
+//                    var toCall_120 = Intent(Intent.ACTION_CALL)
+//                    toCall_120.setData(Uri.parse("tel:120")
+//                    var toCall_120 = Intent(Intent.ACTION_CALL, Uri.parse("tel:120")) // 这里如果用CALL需要开启权限
+                    var toCall_120 = Intent(Intent.ACTION_DIAL, Uri.parse("tel:120")) // 可以用这个
+
+                    startActivity(toCall_120)
+                }
+
+            }
+        }
 
     }
 
